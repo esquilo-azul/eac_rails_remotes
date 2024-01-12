da5195f2b9d5fa9cbde8e962dd0396e70812bedf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-01-12 02:59:05 -0300

    .rubocop.yml#Metrics/MethodLength: remove.

--- spec/support/rails_app_append/db/migrate/20230521030001_create_transactions.rb
+++ spec/support/rails_app_append/db/migrate/20230521030001_create_transactions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreateTransactions < ActiveRecord::Migration[5.2]
-  def change
+  def change # rubocop:disable Metrics/MethodLength
     create_table :transactions do |t|
       t.references :from, index: true
       t.references :to, index: true
