0d3fdbfa8f4e03ff2273d377a69a006c41cff176
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-18 23:24:32 -0300

    EacRailsRemotes: setup with EacRubyUtils::RootModuleSetup.

--- lib/eac_rails_remotes.rb
+++ lib/eac_rails_remotes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils'
+EacRubyUtils::RootModuleSetup.perform __FILE__
 
 module EacRailsRemotes
-  require_sub __FILE__
 end
+
+require 'eac_active_scaffold'
+require 'eac_rails_utils'
+
+require 'eac_rails_remotes/engine'
